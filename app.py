7370b1356ea8b3e787ea007c73d2fa1cc328c583
Tamas Panyi <panyitamas@gmail.com>
2020-02-29 21:39:10 +0100

    Run pylint in .devops venv (Makefile)

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
 def scale(payload):
-    """Scales Payload"""
-    
+    """Scales Payload"""    
     LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
