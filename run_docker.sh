621b4442f2763106a53a2513629885aa633b4a7d
Tamas Panyi <panyitamas@gmail.com>
2020-03-01 20:31:01 +0100

    Final touch for submission

--- run_docker.sh
+++ run_docker.sh
@@ -9,4 +9,7 @@ docker build --tag predictor .
 docker images
 
 # Step 3: 
-docker run predicotor
+docker run predicotor -p 80:80
+
+#Remark:
+#The image was renamed for uploading to docker hub to psnx/udacity-predictor
